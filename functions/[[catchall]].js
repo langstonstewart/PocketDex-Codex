94ed4e197d4ff113f6267f00579dc3080d620a16
Langston Stewart <langston.professional08@gmail.com>
2026-07-06 04:34:33 -0700

    js test

---
+++ functions/[[catchall]].js
@@ -0,0 +1,23 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  const key = url.pathname.slice(1); 
+
+  if (!key) {
+    return new Response("Gateway Active", { status: 200 });
+  }
+
+
+  const object = await context.env.POKEMON_BUCKET.get(key);
+  
+  if (object === null) {
+    return new Response("File Not Found in Bucket", { status: 404 });
+  }
+
+  
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set('Cache-Control', 'public, max-age=31536000, immutable'); // Cache for 1 month
+
+  return new Response(object.body, { headers });
+}
