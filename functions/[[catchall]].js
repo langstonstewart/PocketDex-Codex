9e4773cc964a6ad97ad3c4e8015ad07368041f94
Langston Stewart <langston.professional08@gmail.com>
2026-07-12 23:28:40 -0700

    updated site function

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,22 +1,35 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-
   const key = url.pathname.slice(1); 
 
   if (!key || key === "index.html") {
     return context.next();
   }
 
+  const cache = caches.default;
+  
+  let response = await cache.match(context.request);
+  
+  if (response) {
+    return response;
+  }
+
   const object = await context.env.POKEMON_BUCKET.get(key);
   
   if (object === null) {
-    return new Response("The file listed could not be found. Please try to access a different file. (Format: {dex_number}_{name}.png)", { status: 404 });
+    return new Response(
+      "The file listed could not be found. Please try to access a different file. (Format: {dex_number}_{name}.png)", 
+      { status: 404 }
+    );
   }
 
-  
   const headers = new Headers();
   object.writeHttpMetadata(headers);
-  headers.set('Cache-Control', 'public, max-age=31536000, immutable'); // Cache for 1 year
+  headers.set('Cache-Control', 'public, max-age=31536000, immutable');
 
-  return new Response(object.body, { headers });
+  response = new Response(object.body, { headers });
+
+  context.waitUntil(cache.put(context.request, response.clone()));
+
+  return response;
 }
