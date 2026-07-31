d4bca53cd31d34bd5d5ad9d72fcabe5656dd33b9
Langston Stewart <langston.professional08@gmail.com>
2026-07-30 23:24:52 -0700

    bom removal

--- ttvef.py
+++ ttvef.py
@@ -1,5 +1,2 @@
-with open("set_data_git\\dex_data.json", "r", encoding="utf-8-sig") as f:
-    content = f.read()
-
-with open("set_data_git\\dex_data.json", "w", encoding="utf-8") as f:
-    f.write(content)
+with open("set_data_git\\dex_data.json", "rb") as f:
+    print(f.read(3))
